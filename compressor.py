8a398ef47cba4aa4ac77abcace3e9a3a5b90b5db
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2024-02-02 16:06:43 +0530

    Update compressor.py

--- compressor.py
+++ compressor.py
@@ -9,7 +9,8 @@ def rmvcmnt(t):
     r = ""
     for l in t:
         if fn1:
-
+            if(r==l=="/"):fn1 = 0; t1 = t1[0:len(t1)-1]
+            else: t1+=l
         elif (l=="\n"):fn1=1; t1+=l
         r = l
 
