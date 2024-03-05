23ccd3d9327144b3cf19b58698ece3d2ba3af153
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2024-03-05 17:20:52 +0530

    Update v.py

--- v.py
+++ v.py
@@ -4,6 +4,10 @@
 # http://msdn.microsoft.com/en-us/library/ms646997.aspx
 VSVersionInfo(
   ffi=FixedFileInfo(
+    # filevers and prodvers should be always a tuple with four items: (1, 2, 3, 4)
+    # Set not needed items to zero 0.
+    filevers=(2, 1, 0, 0),
+    prodvers=(2, 0, 0, 0),
 
 
 
